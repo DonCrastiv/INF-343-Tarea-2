b66401200aaf27b11879c5a7b3d7d5f5c498cf27
DonCrastiv <camilo.gonzalezn@sansano.usm.cl>
2021-11-04 21:53:16 -0300

    change lider

--- lider/lider.go
+++ lider/lider.go
@@ -3,10 +3,12 @@ package main
 import (
 	"context"
 	"log"
+	"time"
 	"net"
 	"fmt"
 
-	pb "inf343-tarea-2/protoLiderJugador"
+	pbJugador "inf343-tarea-2/protoLiderJugador"
+	pbName "inf343-tarea-2/protoLiderName"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/peer"
@@ -18,21 +20,25 @@ const (
 )
 
 type server struct {
-	pb.UnimplementedJugadorServer
+	pbJugador.UnimplementedJugadorServer
 }
 
 var jugadorId int32 = 0
 var jugadores []int32
 var ipToid = make(map[net.Addr]int32)
 
-func (s *server) IngresarSolicitud(ctx context.Context, in *pb.Solicitud) (*pb.RespuestaSolicitud, error) {
+func (s *server) IngresarSolicitud(ctx context.Context, in *pbJugador.Solicitud) (*pbJugador.RespuestaSolicitud, error) {
 	jugadorId++
+	
+	p, _ := peer.FromContext(ctx)
+	direccion := p.Addr
+
 	jugadores = append(jugadores, int32(jugadorId))
 	fmt.Printf("%v", jugadores)
-	return &pb.RespuestaSolicitud{Etapa: int32(1)}, nil
+	return &pbJugador.RespuestaSolicitud{Etapa: int32(1)}, nil
 }
 
-func (s *server) EnviarJugada(ctx context.Context, in *pb.Jugada) (*pb.RespuestaJugada, error){
+func (s *server) EnviarJugada(ctx context.Context, in *pbJugador.Jugada) (*pbJugador.RespuestaJugada, error){
 
 	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
@@ -43,6 +49,13 @@ func (s *server) EnviarJugada(ctx context.Context, in *pb.Jugada) (*pb.Respuesta
 	p, _ := peer.FromContext(ctx)
 	direccion := p.Addr
 
+	c := pbName.NewLiderNameServiceClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	rS, err := c.EnviarJugadas(ctx, &pbName.Jugada{IdJugador: , Jugada: , Etapa: })
+	if err != nil {
+		log.Fatalf("Hubo un error con el envío o proceso de la solicitud: %v", err)
+	}
 
 	
 }
@@ -56,7 +69,7 @@ func main() {
 	s := server{}
 	grcpServer := grpc.NewServer()
 
-	pb.RegisterJugadorServer(grcpServer, &s)
+	pbJugador.RegisterJugadorServer(grcpServer, &s)
 	log.Printf("server listening at %v", lis.Addr())
 	if err := grcpServer.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
