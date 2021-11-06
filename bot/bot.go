4fe20994037f57f12b1f921dfa8e9cdcd30a9af3
lilkimo <zarkokuljis.zk@gmail.com>
2021-11-05 22:43:48 -0300

    added bot and input system

--- bot/bot.go
+++ bot/bot.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"math/rand"
 	"os"
-	"strconv"
 	"time"
 
 	"context"
@@ -19,20 +17,6 @@ const (
 	address = "localhost:50051"
 )
 
-func getNumInput(s string, min int, max int) int32 {
-	var input string
-	log.Print(s)
-	fmt.Scanln(&input)
-	v, _ := strconv.Atoi(input)
-	
-	if v < min {
-		return int32(min)
-	} else if v > max {
-		return int32(max)
-	}
-	return int32(v)
-}
-
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
@@ -44,6 +28,10 @@ func main() {
 
 	c := pb.NewJugadorClient(conn)
 
+	/*
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	*/
 	ctx := context.Background()
 	rS, err := c.SolicitarUnirse(ctx, &pb.Unirse{})
 	if err != nil {
@@ -58,11 +46,11 @@ func main() {
 	for !elim {
 		switch etapa {
 		case 1:
-			jugada = getNumInput("Luz Roja, Luz Verde\nIngrese un número entre el 1 y el 10: ", 1, 10)
+			jugada = rand.Int31n(10) + 1
 		case 2:
-			jugada = getNumInput("Tirar la Cuerda\nIngrese un número entre el 1 y el 4: ", 1, 4)
+			jugada = rand.Int31n(4) + 1
 		case 3:
-			jugada = getNumInput("Todo o Nada\nIngrese un número entre el 1 y el 10: ", 1, 10)
+			jugada = rand.Int31n(10) + 1
 		case 4:
 			os.Exit(0)
 		}
